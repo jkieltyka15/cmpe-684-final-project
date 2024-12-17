cd3f530e17e9a60c0c74cecb3328a7a8ffc0790e
jkieltyka15 <jmkielty@mtu.edu>
2024-12-16 19:12:42 -0500

    Fixed auto ack feature. Added persistent message attempts when messages fail to be delivered.

--- base_station/arduino/src/basestation.cpp
+++ base_station/arduino/src/basestation.cpp
@@ -18,12 +18,17 @@
 #include "basestation.hpp"
 
 
+// base station's node ID
+#define BASE_STATION_ID 0
+
+// special base station address since 0x00000000 is not a valid address
+#define BASE_STATION_ADDRESS 0xBAD1DEA5
+
 #define RF24_CHANNEL_SPACING 5  // number of channels between a valid node channel
-#define RF24_READING_PIPE 0     // default reading pipe for the NRF24L01
+#define RF24_READING_PIPE 1     // reading pipe for the NRF24L01
 
-
-#define MAX_SEND_ATTEMPTS 15            // maximum number of attempts to send a message
-#define FAILED_SEND_DELAY_MIN_MS 0      // minimum delay between sending message attempts in milliseconds
+#define MAX_SEND_ATTEMPTS 15    // maximum number of attempts to send a message
+#define FAILED_SEND_DELAY 15    // minimum delay between sending message attempts
 
 
 BaseStation::BaseStation(uint8_t node_id) {
@@ -37,8 +42,15 @@ BaseStation::BaseStation(uint8_t node_id) {
 
 uint32_t BaseStation::calculate_radio_address(uint8_t node_id) {
 
+    // base station has special non-calculated address
+    if (BASE_STATION_ID == node_id) {
+        return BASE_STATION_ADDRESS;
+    }
+
+    // generate address as byte array
     const byte address_bytes[RF24_ADDRESS_WIDTH] = { node_id, node_id, node_id, node_id };
 
+    // convert byte array to 32 bit integer
     uint32_t address = 0;
     memcpy(&address, address_bytes, sizeof(address));
     
@@ -62,9 +74,9 @@ bool BaseStation::init() {
     }
 
     // configure radio
+    radio.enableDynamicPayloads();
     radio.setAutoAck(true);
-    radio.enableAckPayload();
-    radio.setRetries(FAILED_SEND_DELAY_MIN_MS, MAX_SEND_ATTEMPTS);
+    radio.setRetries(FAILED_SEND_DELAY, MAX_SEND_ATTEMPTS);
     radio.setAddressWidth(RF24_ADDRESS_WIDTH);
     radio.setPALevel(RF24_PA_MAX);
     radio.setChannel(this->radio_channel);
--- sensor_node/arduino/src/main.cpp
+++ sensor_node/arduino/src/main.cpp
@@ -35,6 +35,15 @@
 // parking sensor node
 SensorNode node = SensorNode(NODE_ID);
 
+// flag for if this node's update message was successfully sent
+bool is_update_sent = true;
+
+// flag for if a message received was successfully forwarded
+bool is_update_forwarded = true;
+
+// holds the last forwarded message that was attempted to be transmitted
+UpdateMessage update_msg_to_forward = UpdateMessage();
+
 
 /**
  * @brief Initialize all necessary objects and variables.
@@ -66,7 +75,7 @@ void setup() {
 void loop() {
 
     // determine if parking space status has changed
-    if (true == node.is_sensor_status_changed()) {
+    if (true == node.is_sensor_status_changed() || false == is_update_sent) {
 
         // determine recepient
         int16_t rx_id = get_next_ingress_node(node.get_id());
@@ -77,11 +86,27 @@ void loop() {
         }
 
         else {
+
             // transmit update
-            if (false == node.transmit_update((uint8_t)rx_id)) {
+            is_update_sent = node.transmit_update((uint8_t)rx_id);
+
+            // failed to transmit update
+            if (false == is_update_sent) {
 
                 ERROR("Failed to transmit update message to Node " + rx_id)
             }
+        }
+    }
+
+    // attempt to forward message again
+    else if (false == is_update_forwarded) {
+
+        // forward update
+        is_update_forwarded = node.transmit_update(&update_msg_to_forward);
+
+        // failed to forward update
+        if (false == is_update_forwarded) {
+            ERROR("Failed to transmit update message to " + update_msg_to_forward.get_rx_id())
         }
     }
 
@@ -132,10 +157,13 @@ void loop() {
                             uint8_t tx_id = node.get_id();
                             uint8_t node_id = update_msg.get_node_id();
                             bool is_vacant = update_msg.get_is_vacant();
-                            UpdateMessage new_msg = UpdateMessage(rx_id, tx_id, node_id, is_vacant);
+                            update_msg_to_forward = UpdateMessage(rx_id, tx_id, node_id, is_vacant);
 
-                            // forward the message
-                            if (false == node.transmit_update(&new_msg)) {
+                            // forward update
+                            is_update_forwarded = node.transmit_update(&update_msg_to_forward);
+
+                            // failed to forward update
+                            if (false == is_update_forwarded) {
                                 ERROR("Failed to transmit update message to " + rx_id)
                             }
                         }
@@ -153,6 +181,11 @@ void loop() {
 
     // nothing to do
     else {
+        delay(MAIN_LOOP_DELAY_MS);
+    }
+
+    // wait to retry to send message
+    if (false == is_update_sent || false == is_update_forwarded) {
         delay(MAIN_LOOP_DELAY_MS);
     }
 }
